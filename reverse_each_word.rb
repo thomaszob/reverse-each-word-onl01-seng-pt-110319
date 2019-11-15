ba20188a460b622b5e1258f43333ab559026a9e9
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-15 03:24:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   newSent = ""
   counter = 4
-  arrayNew = sentence.split(" ")
-  sentence.split(" ").each{|words|
+  arrayNew = sentence.split()
+  sentence.split().each{|words|
   if counter < arrayNew.count
     newSent << "#{words.reverse }"
     counter += 1
